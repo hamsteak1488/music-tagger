e681ac4f5c3d6b688c4b3ef6413ccda0dfed7441
Dufrane-S <sskim629@gmail.com>
2023-03-05 15:05:31 +0900

    자동 로그인시 팀 로고(임시)가 나오게 변경 최종

--- app/src/main/java/com/cookandroid/myapplication/Loading.kt
+++ app/src/main/java/com/cookandroid/myapplication/Loading.kt
@@ -40,19 +40,14 @@ class Loading : AppCompatActivity() {
                                     startActivity(it)
                                 }
                             }
+                        }
                 }
             }
-        }
-
-
-
-
-    } else {
+        } else {
             Intent(this, LoginActivity::class.java).also {
                 it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(it)
             }
         }
-
-}
+    }
 }
