b44e6425f91ad6cf9b48313b70f1fdcd52e1190a
jm0s <woals1488@naver.com>
2023-05-03 21:53:30 +0900

    jh branch merged

--- app/src/main/java/com/cookandroid/myapplication/MusicServiceConnection.kt
+++ app/src/main/java/com/cookandroid/myapplication/MusicServiceConnection.kt
@@ -36,32 +36,3 @@ object MusicServiceConnection : ServiceConnection {
         musicService = null
     }
 }
-
-/*
-class MusicServiceConnection private constructor() : ServiceConnection {
-
-    companion object {
-        private var instance:MusicServiceConnection? = null
-        var musicService:MusicService? = null
-        private lateinit var context: Context
-
-        fun getInstance(_context:Context):MusicServiceConnection {
-            return instance?: synchronized(this) {
-                instance?:MusicServiceConnection().also {
-                    context = _context
-                    instance = it
-                }
-            }
-        }
-    }
-
-    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-        val serviceBinder = service as MusicService.MyBinder
-        musicService = serviceBinder.currentService()
-    }
-
-    override fun onServiceDisconnected(p0: ComponentName?) {
-        musicService = null
-    }
-}
-*/
